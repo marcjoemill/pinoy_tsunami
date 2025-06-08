6aad866035dca5064bb9c68c987f9c13515ecfa5
marcjoemill <mpmendoza6@up.edu.ph>
2025-06-08 16:47:31 +0800

    feat: increasing difficulty

--- scenes/ebike.gd
+++ scenes/ebike.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-@export var speed: float = 200.0  # You can increase this to make it faster
+@export var speed: float = 200.0  # Base speed
+var speed_multiplier: float = 1.0  # Multiplier from main scene
 
 @onready var animated_sprite = $AnimatedSprite2D
 
@@ -9,4 +10,8 @@ func _ready():
 	animated_sprite.play()
 
 func _process(delta):
-	position.x -= speed * delta
+	position.x -= speed * speed_multiplier * delta
+
+# Called by the main script to adjust difficulty
+func set_speed_multiplier(multiplier: float):
+	speed_multiplier = multiplier
