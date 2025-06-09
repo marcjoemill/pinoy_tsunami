bed06437b8f63424a875965e9c1960f5c944ba43
Lazzzer Gasser <dvmejorada@up.edu.ph>
2025-06-09 10:06:15 +0800

    feat: added game over sprite and fixed input logic

--- scenes/gameover.gd
+++ scenes/gameover.gd
@@ -1,13 +1,19 @@
 extends Control
 
+@onready var animated_sprite = $AnimatedSprite2D
 @onready var score_label = $VBoxContainer/ScoreLabel
 @onready var back_button = $VBoxContainer/BackButton
 @onready var game_over_music = $GameOverMusic
 
+var input_blocked := true
+
 func _ready():
 	show_score(Global.last_score)
+	animated_sprite.play()
 	game_over_music.stream.loop = true
 	game_over_music.play()
+	
+	block_input_for_seconds(12)
 
 func show_score(final_score):
 	score_label.text = "%d" % final_score
@@ -16,3 +22,14 @@ func _input(event):
 	if event is InputEventMouseButton or event is InputEventKey:
 		game_over_music.stop()
 		get_tree().change_scene_to_file("res://scenes/menuscreen.tscn")
+
+func block_input_for_seconds(seconds: float) -> void:
+	var timer := Timer.new()
+	timer.one_shot = true
+	timer.wait_time = seconds
+	add_child(timer)
+	timer.start()
+	timer.timeout.connect(_on_input_unblocked)
+
+func _on_input_unblocked():
+	input_blocked = false
